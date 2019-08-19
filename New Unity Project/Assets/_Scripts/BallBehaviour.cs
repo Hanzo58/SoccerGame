ff71d18dad2e4eb962d3d0a9156e7ac0b68649bf
Donnavan <donnavanw@live.com>
2019-08-20 11:47:28 +1200

    My baddass game

---
+++ New Unity Project/Assets/_Scripts/BallBehaviour.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallBehaviour : MonoBehaviour
+{
+
+    private Rigidbody rb;
+    public float ballThrust;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+
+        rb.AddForce(transform.forward * ballThrust, ForceMode.Acceleration);
+
+
+
+
+    }
+
+}
---
+++ New Unity Project/Assets/_Scripts/Paddleinput.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddleinput : MonoBehaviour
+{
+   
+        public float speed;
+    public float clampValue;
+
+    // Update is called once per frame
+    void Update()
+    {
+
+      
+
+
+        MovePlayer1(Input.GetAxis("Horizontal"));
+
+    }
+
+
+
+    public void MovePlayer1(float amountToMove)
+    {
+        Vector3 clampedGameObjectPosition = gameObject.transform.position + new Vector3(0, 0, amountToMove * speed);
+
+        clampedGameObjectPosition.z = Mathf.Clamp(clampedGameObjectPosition.z, -clampValue, clampValue);
+
+
+
+
+        gameObject.transform.position = clampedGameObjectPosition;
+        
+
+        
+       
+    }
+}
